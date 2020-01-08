26d8117c7a29bcc85f012629bdee285f49efe222
yusheng <yusheng@100tal.com>
2020-01-08 19:43:16 +0800

    day05

---
+++ src/offer/TreeNode.java
@@ -0,0 +1,18 @@
+package offer;
+
+/**
+ * @author: create by ys
+ * @version: v1.0
+ * @description: offer
+ * @date:2020-01-08
+ */
+public class TreeNode {
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
---
+++ src/offer/day05.java
@@ -0,0 +1,11 @@
+package offer;
+
+/**
+ * @author: create by ys
+ * @version: v1.0
+ * @description: offer
+ * @date:2020-01-08
+ */
+public class day05 {
+
+}
