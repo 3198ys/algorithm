4e1ca97abc086b2ae47399f6d901b0d937ae154f
yusheng <yusheng@100tal.com>
2020-01-17 19:08:00 +0800

    day07

---
+++ src/leetcode/Node.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * @author: create by ys
+ * @version: v1.0
+ * @description: leetcode
+ * @date:2020/1/17
+ */
+public class Node {
+
+  public int key,val;
+  public Node next,prev;
+
+  public Node(int k,int v){
+    this.key=k;
+    this.val=v;
+  }
+}
