5023623582241c953a49a0bc73bf0e97a9d145ff
yusheng <yusheng@100tal.com>
2020-03-25 00:28:22 +0800

    双指针

---
+++ src/offer/day37.java
@@ -0,0 +1,19 @@
+package offer;
+
+import java.util.ArrayList;
+
+/**
+ * @author: create by ys
+ * @version: v1.0
+ * @description: 输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
+ *
+ * 利用双指针来做
+ * @date:2020/3/25
+ */
+public class day37 {
+  public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+
+  }
+
+  public void digui(ArrayList<ArrayList<Integer>> list,ArrayList<Integer> tmqList,)
+}
