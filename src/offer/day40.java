a66b07f2b08447e214e320962e577e91ccc3d787
yusheng <yusheng@100tal.com>
2020-04-02 00:06:17 +0800

    字符串翻转

--- src/offer/day40.java
+++ src/offer/day40.java
@@ -37,4 +37,20 @@ public class day40 {
     }
     return a;
   }
+
+  public String ReverseSentenceV2(String str) {
+    if(str.trim().equals("")){
+      return str;
+    }
+    String[] a = str.split(" ");
+    StringBuffer o = new StringBuffer();
+    int i;
+    for (i = a.length; i >0;i--){
+      o.append(a[i-1]);
+      if(i > 1){
+        o.append(" ");
+      }
+    }
+    return o.toString();
+  }
 }
