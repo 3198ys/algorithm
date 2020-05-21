2d9e44c4cc483ffe764a647c22fe55e27a87d6b6
yusheng <yusheng@100tal.com>
2020-05-22 00:36:07 +0800

    枚举单列

---
+++ src/pattern/single/register/EnumSingleton.java
@@ -0,0 +1,12 @@
+package pattern.single.register;
+
+/**
+ * @author: create by ys
+ * @version: v1.0
+ * @description: pattern.single.register
+ * 使用枚举类来做单列
+ * @date:2020/5/22
+ */
+public enum EnumSingleton {
+
+}
---
+++ src/pattern/single/register/User.java
@@ -0,0 +1,11 @@
+package pattern.single.register;
+
+/**
+ * @author: create by ys
+ * @version: v1.0
+ * @description: pattern.single.register
+ * @date:2020/5/22
+ */
+public class User {
+
+}
