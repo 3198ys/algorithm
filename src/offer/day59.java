36c6cb33ff2b8a879bbc7f1ab3c12cdc53be1575
yusheng <18712782695@163.com>
2020-05-22 14:25:10 +0800

    请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。

---
+++ src/offer/day59.java
@@ -0,0 +1,10 @@
+package offer;
+
+/**
+ * @author: create by ys
+ * @version: v1.0
+ * @description:请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
+ * @date:2020/5/22
+ */
+public class day59 {
+}
