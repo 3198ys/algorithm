b7f535a6792c2267f8fd44b3ce885236562767c7
yusheng <yusheng@rd.netease.com>
2020-07-21 14:11:37 +0800

    双指针 链表是否有环 使用双指针快慢指针来做

---
+++ src/leetcode/双指针/ListNode.java
@@ -0,0 +1,14 @@
+package leetcode.双指针;
+
+/**
+ * @Author ys 2020/7/21
+ * @description
+ */
+public class ListNode {
+  int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+}
