55c4d1daae877fbc980f432ee540969eea94c111
ryanyu    <ryanyu@didiglobal.com>
2025-04-01 09:17:38 +0800

    连表排序

--- src/offer/danlianbiaopaix.java
+++ src/offer/danlianbiaopaix.java
@@ -41,4 +41,39 @@ public class danlianbiaopaix{
         }
         return aux.next;
     }
+
+
+    public static ListNode sortv2(ListNode root){
+        if(root == null || root.next == null){
+            return root;
+        }
+        //接下来的主要思路就是从头到尾的的循环找出当前节点应该在的位置
+        ListNode curr=root.next; // 需要比较的节点
+        ListNode pre = root;//前一个节点
+        ListNode listNode = new ListNode(-1); //后续为了方便遍历整个连表的节点
+        listNode.next = root;
+        while (curr !=null){
+            if(curr.val<pre.val){
+                //如果当前节点小于前一个节点的话就需要去调整位置，主要的调整方式就是首先去要在连表上把当前节点去掉，然后在链表中循环找到改节点应该在的位置
+                pre.next = curr.next;
+                ListNode node1=listNode;
+                ListNode node2 = listNode.next;
+                while (curr.val>node2.val){
+                    //如果当前节点大于node2的节点就要一直向下遍历，为了找到 curr<node2的然后插入node2之前
+                    node1=node2;
+                    node2=node2.next;
+                }
+                //如果跳出了循环说明现在 curr已经小于了node2但是大于了node1现在就可以插入了
+                node1.next = curr;
+                curr.next = node2;
+                //现在就需要将当前节点向下移动一位了
+                curr = pre.next;
+            }else{
+                //如果当前节点已经大于pre节点那么现在就需要一次向下了
+                pre = curr;
+                curr= curr.next;
+            }
+        }
+        return listNode.next;
+    }
 }
